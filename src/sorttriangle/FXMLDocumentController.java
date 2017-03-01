a3914edff93ad74cdfd10deb1af1b261e120afa9
blj0011 <blj0011@LIB-AU23918.ad.auburn.edu>
2017-03-01 11:28:05 -0600

    Merge origin/Diver_latest into second
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/diverapp/Diver.java
    	src/diverapp/FXMLDocumentController.java
    	src/diverapp/Harpoon.java
    	src/diverapp/Squid.java

--- src/sorttriangle/FXMLDocumentController.java
+++ src/sorttriangle/FXMLDocumentController.java
@@ -6,8 +6,7 @@
 package sorttriangle;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.ResourceBundle;
+import java.util.*;
 import javafx.animation.ParallelTransition;
 import javafx.animation.PathTransition;
 import javafx.animation.SequentialTransition;
@@ -23,7 +22,6 @@ import javafx.scene.shape.CubicCurveTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.util.Duration;
-
 /**
  *
  * @author blj0011
@@ -39,6 +37,8 @@ public class FXMLDocumentController implements Initializable {
         
     int[] circleValues = {4,3,1,2};
     final Circle[] circleContainer = {circle1, circle2, circle3, circle4};
+    Bounds[] circleBounds = new Bounds[4];
+    int[] indexContainer = {0, 1, 2, 3};
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -53,24 +53,29 @@ public class FXMLDocumentController implements Initializable {
         circle1.setCenterX(50);
         circle1.setCenterY(50);
         circle1.setId("4");
+        circleBounds[0] = circle1.localToScene(circle1.getBoundsInLocal());
+        
         
         circle2.setFill(Color.RED);
         circle2.setRadius(20);
         circle2.setCenterX(100);
         circle2.setCenterY(50);
         circle2.setId("3");
+         circleBounds[1] = circle1.localToScene(circle2.getBoundsInLocal());
         
         circle3.setFill(Color.GREEN);
         circle3.setRadius(20);
         circle3.setCenterX(150);
         circle3.setCenterY(50);
         circle3.setId("1");
+        circleBounds[2] = circle1.localToScene(circle3.getBoundsInLocal());
         
         circle4.setFill(Color.YELLOW);
         circle4.setRadius(20);
         circle4.setCenterX(200);
         circle4.setCenterY(50);
-        circle4.setId("2");        
+        circle4.setId("2");      
+        circleBounds[3] = circle1.localToScene(circle4.getBoundsInLocal());
              
         //gpMain.getChildren().addAll(circleContainer);
         apMain.getChildren().addAll(circleContainer);
@@ -87,35 +92,34 @@ public class FXMLDocumentController implements Initializable {
             }      
             int smallerNumber = circleValues[index]; 
             Circle smallerCircle = circleContainer[index];
+            //Bounds smallerBounds = circleBounds[index];
             
             circleValues[index] = circleValues[i];
             circleContainer[index] = circleContainer[i];
+            //circleBounds[index] = circleBounds[i];
             
             circleValues[i] = smallerNumber;  
             circleContainer[i] = smallerCircle;
+            //circleBounds[i] = smallerBounds;
             
-            sequentialTransition.getChildren().addAll(swapBalls(circleContainer[i], circleContainer[index]));
+            System.out.println(Arrays.toString(circleBounds));
+            sequentialTransition.getChildren().addAll(swapBalls(i, index));
         }
         
         sequentialTransition.setCycleCount(1);
         sequentialTransition.play();
     }    
     
-    ParallelTransition swapBalls(Circle c1, Circle c2)
+    ParallelTransition swapBalls(int c1Index, int c2Index)
     {
-        Bounds c1Bounds = c1.localToScene(c1.getBoundsInLocal());
-        Bounds c2Bounds = c2.localToScene(c2.getBoundsInLocal());
-        
-        System.out.println(c1Bounds.toString());
-        System.out.println(c2Bounds.toString());
-        Path path = moveTo(c1Bounds, c2Bounds);//path used to move circle from initial postion to final position
-        Path path2 = moveFrom(c2Bounds, c1Bounds);//path used to move cicle in final posion to inital position
+        Path path = moveTo(circleBounds[c1Index], circleBounds[c2Index]);//path used to move circle from initial postion to final position
+        Path path2 = moveFrom(circleBounds[c2Index], circleBounds[c1Index]);//path used to move cicle in final posion to inital position
         
         //Take circle1 and move it into circle2's position.
         final PathTransition pathTransition = new PathTransition();
         pathTransition.setDuration(Duration.seconds(2));
         pathTransition.setPath(path);
-        pathTransition.setNode(c1);
+        pathTransition.setNode(circleContainer[indexContainer[c1Index]]);
         pathTransition.setOrientation(PathTransition.OrientationType.NONE);
         pathTransition.setCycleCount(1);
         pathTransition.setAutoReverse(false);
@@ -125,38 +129,37 @@ public class FXMLDocumentController implements Initializable {
         final PathTransition pathTransition2 = new PathTransition();
         pathTransition2.setDuration(Duration.seconds(2));
         pathTransition2.setPath(path2);
-        pathTransition2.setNode(c2);
+        pathTransition2.setNode(circleContainer[indexContainer[c2Index]]);
         pathTransition2.setOrientation(PathTransition.OrientationType.NONE);
         pathTransition2.setCycleCount(1);
         pathTransition2.setAutoReverse(false);
         //pathTransition2.play();
         
+        //Make the swap happen at the same time
         ParallelTransition pt = new ParallelTransition();
         pt.getChildren().addAll(pathTransition, pathTransition2);
        
         return pt;
     }
     
-    
-    //I personally would add inputs for initial position x, y, and ending position x, y.
     Path moveTo(Bounds c1Bounds, Bounds c2Bounds)
     {
         double c1CenterX = (c1Bounds.getMinX() + c1Bounds.getMaxX()) / 2;
         double c1CenterY = (c1Bounds.getMinY() + c1Bounds.getMaxY()) / 2;
         double c2CenterX = (c2Bounds.getMinX() + c2Bounds.getMaxX()) / 2;
         double c2CenterY = (c2Bounds.getMinY() + c2Bounds.getMaxY()) / 2;
-        
+
         MoveTo moveTo = new MoveTo();
-        moveTo.setX(c1CenterX);//initial x
-        moveTo.setY(c1CenterY);//initial y
+        moveTo.setX(c2CenterX);//initial x
+        moveTo.setY(c2CenterY);//initial y
 
         CubicCurveTo cubicTo = new CubicCurveTo();
         cubicTo.setControlX1(c1CenterX);//initial x
-        cubicTo.setControlY1(c1CenterY - 35);//height - set this. initial y - 35
+        cubicTo.setControlY1(c1CenterY + 35);//height - set this. initial y - 35
         cubicTo.setControlX2(c2CenterX);//ending x
-        cubicTo.setControlY2(c1CenterY - 35);//height - set this. initial y - 35
-        cubicTo.setX(c2CenterX);//ending x
-        cubicTo.setY(c2CenterY);//ending y
+        cubicTo.setControlY2(c1CenterY + 35);//height - set this. initial y - 35
+        cubicTo.setX(c1CenterX);//ending x
+        cubicTo.setY(c1CenterY);//ending y
         
         Path path = new Path();
         path.getElements().add(moveTo);
@@ -173,16 +176,16 @@ public class FXMLDocumentController implements Initializable {
         double c2CenterY = (c2Bounds.getMinY() + c2Bounds.getMaxY()) / 2;
         
         MoveTo moveTo = new MoveTo();
-        moveTo.setX(c1CenterX);//initial x
-        moveTo.setY(c1CenterY);//initial y
+        moveTo.setX(c2CenterX);//initial x
+        moveTo.setY(c2CenterY);//initial y
 
         CubicCurveTo cubicTo = new CubicCurveTo();
         cubicTo.setControlX1(c1CenterX);//initial x
-        cubicTo.setControlY1(c1CenterY + 35);//height - set this. I did initial y + 35
+        cubicTo.setControlY1(c1CenterY - 35);//height - set this. I did initial y + 35
         cubicTo.setControlX2(c2CenterX);//ending x
-        cubicTo.setControlY2(c1CenterY + 35);//height - set this. I did initial y + 35
-        cubicTo.setX(c2CenterX);//ending x
-        cubicTo.setY(c2CenterY);//ending y
+        cubicTo.setControlY2(c1CenterY - 35);//height - set this. I did initial y + 35
+        cubicTo.setX(c1CenterX);//ending x
+        cubicTo.setY(c1CenterY);//ending y
         
         Path path = new Path();
         path.getElements().add(moveTo);
